b678aa3fc7508c60e7d639f54a12d85cafc30950
IVAN LEONI VILAS BOAS <51962986+IvanEng2019@users.noreply.github.com>
2023-10-18 11:39:21 -0300

    Create convert.h

---
+++ UNIFEI_CODIGOS/CODIGOS C_COM111 E COM112_EDI&EDII/ARVORE EM C/Rubro Negra e 234/convert.h
@@ -0,0 +1,8 @@
+#ifndef CONVERT_H
+#define CONVERT_H
+
+void convert234intoRB(TreeRB *ArvRB,Node234 *no234,Node234 *noPai234,NodoRB *noPaiRB);
+TreeRB* convertToRBTree(Tree234 *Arv234);
+
+#endif /* CONVERT_H */
+
