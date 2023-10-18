98c95452fe372ef08baa8ebada9394e383dd257b
IVAN LEONI VILAS BOAS <51962986+IvanEng2019@users.noreply.github.com>
2023-10-18 11:39:34 -0300

    Create menu.c

---
+++ UNIFEI_CODIGOS/CODIGOS C_COM111 E COM112_EDI&EDII/ARVORE EM C/Rubro Negra e 234/menu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "menu.h"
+
+void menu123Tree(){
+    printf("\nÁrvores Balanceadas : Menu\n");
+    printf("1. Inserir novo elemento na árvore 2-3-4\n");
+    printf("2. Remover elemento da árvore 2-3-4\n");
+    printf("3. Imprimir árvore 2-3-4\n");
+    printf("4. Converter em uma árvore rubro-negra\n");
+    printf("5. Sair\n");
+}
+
+void menuRBTree(){
+    printf("\nÁrvores Balanceadas : Menu\n");
+    printf("1. Inserir novo elemento na árvore rubro-negra\n");
+    printf("2. Remover elemento da árvore rubro-negra\n");
+    printf("3. Imprimir árvore rubro-negra\n");
+    printf("4. Sair\n\n");
+}
